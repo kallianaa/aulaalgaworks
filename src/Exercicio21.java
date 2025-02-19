a97a5756ecfe045e7edfc247dc5add55552dd05f
Kalliana Glaeser <kalliana.glaeser@gmail.com>
2025-02-19 19:54:44 -0300

    aula8

---
+++ src/Exercicio21.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Exercicio21 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+
+
+        scanner.close();
+    }
+
+
+
+
+
+
+}
