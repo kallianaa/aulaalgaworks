20359691dacc61dc43607c695a8f22ad3644c90b
Kalliana Glaeser <kalliana.glaeser@gmail.com>
2025-02-25 21:16:56 -0300

    aula de remoção de elementos

--- src/listadinamica/AlgoritmoAvancado.java
+++ src/listadinamica/AlgoritmoAvancado.java
@@ -5,29 +5,70 @@ public class AlgoritmoAvancado {
     public static void main(String[] args) {
         ListaAlunos lista = new ListaAlunos();
 
-        Aluno aluno = new Aluno();
-        aluno.nome = "João";
-        lista.adicionar(aluno);
+        Aluno aluno1 = new Aluno();
+        aluno1.nome = "João";
+        lista.adicionar(aluno1);
 
-        aluno = new Aluno();
-        aluno.nome = "Maria";
-        lista.adicionar(aluno);
+        Aluno aluno2 = new Aluno();
+        aluno2.nome = "Maria";
+        lista.adicionar(aluno2);
 
-        aluno = new Aluno();
-        aluno.nome = "Alexandre";
-        lista.adicionar(aluno);
+        Aluno aluno3 = new Aluno();
+        aluno3.nome = "Alexandre";
+        lista.adicionar(aluno3);
 
-        aluno = new Aluno();
-        aluno.nome = "Normandes";
-        lista.adicionar(aluno);
+        Aluno aluno4= new Aluno();
+        aluno4.nome = "Normandes";
+        lista.adicionar(aluno4);
 
-        aluno = new Aluno();
-        aluno.nome = "Thiago";
-        lista.adicionar(aluno);
+        Aluno aluno5 = new Aluno();
+        aluno5.nome = "Thiago";
+        lista.adicionar(aluno5);
 
         for (int i = 0; i < lista.tamanho(); i++) {
             Aluno a = lista.obter(i);
-            System.out.println("Aluno: " + a.nome);
+            if (a != null) {
+                System.out.println("Aluno: " + a.nome);
+            } else {
+                System.out.println("Aluno sem nome");
+            }
         }
+
+        System.out.println("--------------------------");
+
+        lista.remover(aluno3);
+
+        for (int i = 0; i < lista.tamanho(); i++) {
+            Aluno a = lista.obter(i);
+            if (a != null) {
+                System.out.println("Aluno: " + a.nome);
+            } else {
+                System.out.println("Aluno sem nome");
+            }
+        }
+//        Aluno aluno = new Aluno();
+//        aluno.nome = "João";
+//        lista.adicionar(aluno);
+//
+//        aluno = new Aluno();
+//        aluno.nome = "Maria";
+//        lista.adicionar(aluno);
+//
+//        aluno = new Aluno();
+//        aluno.nome = "Alexandre";
+//        lista.adicionar(aluno);
+//
+//        aluno = new Aluno();
+//        aluno.nome = "Normandes";
+//        lista.adicionar(aluno);
+//
+//        aluno = new Aluno();
+//        aluno.nome = "Thiago";
+//        lista.adicionar(aluno);
+//
+////        for (int i = 0; i < lista.tamanho(); i++) {
+////            Aluno a = lista.obter(i);
+////            System.out.println("Aluno: " + a.nome);
+////        }
     }
 }
